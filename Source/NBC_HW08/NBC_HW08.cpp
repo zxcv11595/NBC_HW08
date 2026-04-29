e0b5c670e6fadfd7040c685078cec395a389c6d8
zxcv11595 <zxcv11595@gmail.com>
2026-04-30 03:10:20 +0900

    Initial commit

---
+++ Source/NBC_HW08/NBC_HW08.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_HW08.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_HW08, "NBC_HW08" );
